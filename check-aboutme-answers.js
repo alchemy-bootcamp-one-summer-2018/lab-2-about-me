1a5c16b4c52c53ccb35b1c63d29521f50a5adcb5
Alex Rankin <alex.c.rankin@gmail.com>
2018-07-24 15:34:33 -0700

    starting to build js

---
+++ check-aboutme-answers.js
@@ -0,0 +1,11 @@
+
+var factForm = document.getElementById('facts-form');
+var aboutMeResults = document.getElementById('aboutMe-results');
+var aboutMeSubmit = document.getElementById('aboutMe-submit');
+var aboutMeReset = document.getElementById('aboutMe-reset');
+
+function checkAboutMeAnswers(){
+    console.log('hey bud');
+
+    return false;
+}
