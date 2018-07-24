5e1c14fd151c475f4024b509d3f12a7bfd584359
Alex Rankin <alex.c.rankin@gmail.com>
2018-07-24 16:08:34 -0700

    testing questions

--- check-aboutme-answers.js
+++ check-aboutme-answers.js
@@ -8,13 +8,40 @@ function checkAboutMeAnswers(){
     console.log('hey bud');
 
     var elements = factForm.elements;
-
     var hometown = elements.hometown.value;
-
     var glassesAge = elements.glassesAge.value;
+    var collegePlace = elements.collegePlace.value;
+    var numOfSibs = elements.numOfSibs.value;
+    var career = elements.career.value;
 
     console.log('hometown', hometown);
     console.log('glassesAge', glassesAge);
-    
+    console.log('collegePlace', collegePlace);
+    console.log('numOfSibs', numOfSibs);
+
+    var correct = 0;
+
+    if(hometown.toLowerCase() === 'la' || hometown.toLowerCase() === 'los angeles') {
+        console.log('way to go');
+        correct = correct + 1;
+    }
+    if(glassesAge == 10 || glassesAge.toLowerCase() === 'ten') {
+        console.log('hey thats right');
+        correct = correct + 1;
+    }
+    if(collegePlace.toLowerCase() === 'ucsb' || collegePlace.toLowerCase() === 'santa barbara'){
+        console.log('sb was awful');
+        correct = correct + 1;
+    }
+    if(numOfSibs === 'oneBro'){
+        console.log('one sweet bro');
+        correct = correct + 1;
+    }
+    if(career === 'coffee'){
+        console.log('that was then');
+        correct = correct +1;
+    }
+
+
     return false;
 }
