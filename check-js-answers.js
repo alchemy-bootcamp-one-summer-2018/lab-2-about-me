8eb5ba119f2b43ded0faa91ba0d17da2a1bb07b5
Claire Flanagan <claireflanagan@gmail.com>
2018-07-24 16:55:35 -0700

    two working questions on first quiz

--- check-js-answers.js
+++ check-js-answers.js
@@ -1,72 +1,38 @@
-/* exported checkJSAnswers, resetJSAnswers */
+var factsForm = document.getElementById('facts-form');
+var factsSubmit = document.getElementById('js-submit');
+var factsReset = document.getElementById('js-reset');
+var factsResults = document.getElementById('js-results')
 
-// Reference all the elements (by id) that we are going
-// to need to use in our functions
-var factForm = document.getElementById('facts-form');
-var factResults = document.getElementById('js-results');
-var factSubmit = document.getElementById('js-submit');
-var factReset = document.getElementById('js-reset');
-
-// function that will run when "submit" event happens on 
-// form (because we put onsubmit="return checkJSAnswers()")
 function checkJSAnswers() {
-
-    // PART 1 - GET THE VALUES
-
-    // log some details about fact that function is being run
-    // add take a look at form output
     console.log('checkJSAnswers called');
-    console.dir(factForm);
-    // form exposes a special "elements" property (just part of how forms
-    // work in the browser). This gives us access to all the input controls,
-    // so we can get the values out of them.
-    var elements = factForm.elements;
+    var elements = factsForm.elements;
+    var vegetable = elements.vegetable.value;
+    var misdemeanors = elements.misdemeanors.value;
 
-    // we can access the value of any input by using it's "value" property
-    // a text input has the value in the control that the user typed
-    var inventor = elements.inventor.value;
-    // a radio group has the value from the input that is checked
-    var runInBrowser = elements.runInBrowser.value;
+    console.log('vegetable', vegetable);
+    console.log('misdemeanors', misdemeanors);
 
-    // inspecting those value to make sure we got it
-    console.log('inventor', inventor);
-    console.log('runInBrowser', runInBrowser);
-
-    // PART 2 - Check those answers (values)
-
-    // keep count of correct answers in a variable
     var correct = 0;
 
-    // check answer 1
-    if(inventor.toLowerCase() === 'eich') {
-        // if correct, add 1 to correct count
+    if (vegetable.toLowerCase() === 'yes') {
         correct = correct + 1;
-    }
-    
-    // check answer 2
-    if(runInBrowser.toLowerCase() === 'yes') {
-        // increment the correct count
+    } //so I'm calling a function here???
+
+    if (misdemeanors.toLowerCase() === 'yes') {
         correct = correct + 1;
     }
 
-    // PART 3 - Report results to the user
+    factsResults.innerText = 'Congrats! You got ' + correct + ' out of 5 correct';
+    factsSubmit.disabled = true;
+    factsReset.disabled = false;
 
-    // inner text for freeform text response
-    factResults.innerText = 'You got ' + correct + ' out of 2 right';
-    // disable the submit button (because game is done)
-    factSubmit.disabled = true;
-    // enable the reset button (because it is the Play Again button)
-    factReset.disabled = false;
+    function resetJSAnswers() {
+        factsResults.innerText = '';
+        factsSubmit.disabled = false;
+        factReset.disabled = true;
+    }
 
-    // we need this to keep the form from changing/refreshing the webpage
+
     return false;
-}
 
-// function that will run when a reset event happens on the form
-// via onreset="resetJSAnswers()"
-function resetJSAnswers() {
-    // put everything back to how it was at start of game
-    factResults.innerText = '';
-    factSubmit.disabled = false;
-    factReset.disabled = true;
 }
