f7be1b79804ca9564520e557b1c99c3606b87757
Dyanna Tolman <dyannast@gmail.com>
2018-07-24 17:29:53 -0700

    more lines

--- check-js-answers.js
+++ check-js-answers.js
@@ -1,3 +1,16 @@
+var factForm = document.getElementById('facts-form');
+var factResults = document.getElementById('js-results');
+
 function checkJSAnswers() {
     console.log('checkJSAnswers called');
+
+var elements = factForm.elements;
+var dessert = elements.dessert.value;
+console.log('dessert', dessert);
+var correct = 0;
+if(dessert.toLowerCase() === 'cake'){
+    correct = correct + 1;
+}
+factResults.innerText = 'You got ' + correct + ' out of 5 right';
+return false;
 }
