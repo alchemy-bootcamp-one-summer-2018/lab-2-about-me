51e97934c5e9cf4bd094696456d683c3d8658c89
Sophie Graham <sophiergraham1@gmail.com>
2018-07-24 17:31:10 -0700

    displays correct answer

--- checkJSAnswers.js
+++ checkJSAnswers.js
@@ -1,3 +1,20 @@
-function checkJSAnswers(){
+var factForm = document.getElementById('facts-form');
+var factResults = document.getElementById('js-results');
+
+function checkJSAnswers() {
     console.log('checkJSAnswers called');
+
+    var elements = factForm.elements;
+    var hometown = elements.hometown.value;
+ 
+    console.log('hometown', hometown);
+    
+    var correct = 0;
+
+    if(hometown.toLowerCase() === 'portland') {
+        correct = correct + 1;
+    }
+    console.log('results')
+    factResults.innerText = 'You got ' + correct + ' out of 5 right';
+    return false;
 }
