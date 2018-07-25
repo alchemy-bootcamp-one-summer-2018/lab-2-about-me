33cc36c1c115bb9edbb3d960d1fd2b834ae9ed8b
Sarah Flynn <sarah.flynn@icloud.com>
2018-07-25 15:50:35 -0700

    everything working

--- count-tags.js
+++ count-tags.js
@@ -1,5 +1,10 @@
 /* exported countTags, resetTagCounter */
 
+// Declaring html variable to be used to post results
+var html = '<h4>Done! The following list shows the numbers of each HTML element type you selected:</h4><ul>';
+
+var countResult = document.getElementById('tag-count-results');
+
 function countTags() {
 
     // Test to make sure function is working
@@ -11,11 +16,6 @@ function countTags() {
 
     // Declaring an empty object to store tag counts
     var tagCount = {};
-
-    // Declaring html variable to be used to post results
-    var html = '<ul>';
-
-    var countResult = document.getElementById('tag-count-results');
 
     // Looping through each item in the tags object
     for(var i = 0; i < tags.length; i++) {
@@ -32,22 +32,29 @@ function countTags() {
         tagCount[tag] = matches.length;
         console.log('tagCount: ', tagCount);
 
-        // // Get key from tagCount
-        // var key = Object.keys(tagCount)[0];
-        // console.log('key: ', key);
-
         // Append list item in html
         html += '<li>' + tag + ': ' + tagCount[tag] + '</li>';
+        if(i === (tags.length - 1)) {
+            html += '</ul>';
+        }
         console.log(html);
 
         countResult.innerHTML = html;
+
     }
+
+    // Disable submit and enable reset
+    document.getElementById('count-submit').disabled = true;
+    document.getElementById('count-reset').disabled = false;
 
     // Prevents browser window from refreshing automatically
     return false;
 }
 
 function resetTagCounter() {
-
+    document.getElementById('count-submit').disabled = false;
+    document.getElementById('count-reset').disabled = true;
+    countResult.innerHTML = '';
+    html = '<h4>Done! The following list shows the numbers of each HTML element type you selected:</h4><ul>';
 }
 
