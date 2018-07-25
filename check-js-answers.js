ea33959ff4778a5b6436a0651d105efd8a116115
Mike <mbennettillustration@gmail.com>
2018-07-24 17:17:29 -0700

    question 3

--- check-js-answers.js
+++ check-js-answers.js
@@ -1,23 +1,39 @@
+/* exported checkJSAnswers, resetJSAnswers */
+
 var factsForm = document.getElementById('facts-form');
+var factResults = document.getElementById('js-results');
+var factSubmit = document.getElementById('js-submit');
+var factReset = document.getElementById('js-reset');
 
 function checkJSAnswers() {
     console.log('checkJSAnswers called');
     console.dir(factsForm);
+
     var elements = factsForm.elements;
     var state = elements.state.value;
     var runInBrowser = elements.runInBrowser.value;
+    var condiment = elements.condiment.value;
+
     console.log('state', state);
     console.log('runInBrowser', runInBrowser);
+    console.log('condiment', condiment);
+
     var correct = 0;
-}
     if(state.toLowerCase() === 'massachusetts') {
         correct = correct + 1;
     }
-        factResults.innerText = 'Congrats! You got ' + correct + ' out of 2 right!'
-        factSubmit.disabled = true;
-        factReset.disabled = false;
+    if(runInBrowser.toLowerCase() === 'yes') {
+        correct = correct + 1;
+    }
+    if(condiment.toLowerCase() === 'ranch') {
+        correct = correct + 1;
+    }
 
-        return false;
+    factResults.innerText = 'Congrats! You got ' + correct + ' out of 3 right!';
+    factSubmit.disabled = true;
+    factReset.disabled = false;
+    return false;
+}
 function resetJSAnswers() {
     factResults.innerText = '';
     factSubmit.disabled = false;
