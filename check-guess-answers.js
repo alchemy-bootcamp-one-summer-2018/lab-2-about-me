c1365300998ca040bbf60fca7f5baf31869beef7
Claire Flanagan <claireflanagan@gmail.com>
2018-07-24 17:57:15 -0700

    bonus question added

---
+++ check-guess-answers.js
@@ -0,0 +1,20 @@
+/* exported checkGuessAnswer */
+
+var guessForm = document.getElementById('guess-form');
+var guessResults = document.getElementById('guess-results');
+
+var totalGuesses = 0;
+
+function checkGuessAnswer() {
+    var guess = guessForm.elements.florida.value;
+    if (guess == 4) {
+        guessResults.innerText = 'Congrats! You got it correct!';
+    }
+    else {
+        guessResults.innerText = 'Wrong';
+        totalGuesses = totalGuesses + 1;
+        console.log(totalGuesses);
+    }
+
+    return false;
+}
