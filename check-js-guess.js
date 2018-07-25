7f632113277a136972d408e9f54b7e6e159a88d0
kkrosefields <kkrosefields@gmail.com>
2018-07-25 12:18:47 -0700

    linting complete

--- check-js-guess.js
+++ check-js-guess.js
@@ -1,32 +1,26 @@
-/* Exported checkGuessAnswer */
+/* exported checkGuessAnswer */
 
-var guessForm = document.getElementById ("guess-form");
-var guessResult = document.getElementById ("guess-result");
-
+var guessForm = document.getElementById ('guess-form');
+var guessResult = document.getElementById ('guess-result');
 
 var totalGuesses = 0;
-
-
-
-function checkGuessAnswer() {
+function checkGuessAnswer(){
 
     var guess = guessForm.elements.otter.value;
 
-    if(guess === 'otter'); {
+    if(guess === 'otter') {
         guessResult.innerText = 'That\'s right! An ' + guess + ' is my favorite!';
-        console.log ('this is what user guessed', guess)
+        console.log ('this is what user guessed', guess);
     }
-
-    else{
+    else {
 
         totalGuesses = totalGuesses + 1;
-        console.log (totalGuesses)
-        guessResult.innerText = 'Womp Womp... a ' + guess + 
-        ' is not my favorite. You\'re closer though. You\'ve guessed ' + 
-        totalGuesses + ' times try again!'
+        console.log (totalGuesses);
+        guessResult.innerText = 'Whomp Whomp... a ' + guess +
+        ' is not my favorite. You\'re closer though. You\'ve guessed ' +
+        totalGuesses + ' times try again!';
     }
 
-    
     return false;
 
 }
