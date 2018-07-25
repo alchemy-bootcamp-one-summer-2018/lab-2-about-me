3e1806bf68609f361824561026a581285c40a3e2
Alex Rankin <alex.c.rankin@gmail.com>
2018-07-25 13:56:21 -0700

    first steps of counter function

---
+++ tag-checker-answer.js
@@ -0,0 +1,13 @@
+/* exported tagCounter */
+
+tagCounter = function(){
+    console.log('hello');
+
+    return false;
+};
+
+var tags = document.querySelectorAll('input[name=tags]:checked');
+
+var tagCount = {};
+
+
