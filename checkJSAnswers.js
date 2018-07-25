95187577b2eebe8d6c0fc76cee7fa567b2d8e8cf
Sophie Graham <sophiergraham1@gmail.com>
2018-07-25 02:46:29 -0700

    fixed js errors

--- checkJSAnswers.js
+++ checkJSAnswers.js
@@ -8,10 +8,14 @@ function checkJSAnswers() {
     var hometown = elements.hometown.value;
     var runInBrowser = elements.runInBrowser.value;
     var hobby = elements.hobby.value;
+    var family = elements.family.value;
+    var season = elements.season.value;
  
     console.log('hometown', hometown);
     console.log('runInBrowser', runInBrowser);
     console.log('hobby', hobby);
+    console.log('family', family);
+    console.log('season', season);
     var correct = 0;
 
     if(hometown.toLowerCase() === 'portland') {
@@ -23,9 +27,15 @@ function checkJSAnswers() {
     if(hobby.toLowerCase() === 'kayak') {
         correct = correct + 1;
     }
+    if(family.toLowerCase() === 'mississippi') {
+        correct = correct + 1;
+    }
+    if(season.toLowerCase() === 'summer') {
+        correct = correct + 1;
+    }
   
 
     console.log('results1')
-    factResults.innerText = 'You got ' + correct + ' out of 5 right';
+    factResults.innerText = 'You got ' + correct + ' out of 5 right!';
     return false;
     }
