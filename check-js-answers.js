629ba5391f46c2eadaf1820deebf33b9ea01ad20
Dyanna Tolman <dyannast@gmail.com>
2018-07-24 18:40:43 -0700

    more lines

--- check-js-answers.js
+++ check-js-answers.js
@@ -1,14 +1,24 @@
 var factForm = document.getElementById('facts-form');
 var factResults = document.getElementById('js-results');
+var factSubmit = document.getElementById('js-submit');
+var factReset = document.getElementById('js-reset')
 
 function checkJSAnswers() {
     console.log('checkJSAnswers called');
 
 var elements = factForm.elements;
 var dessert = elements.dessert.value;
+var runInBrowser = elements.runInBrowser.value;
+
 console.log('dessert', dessert);
+console.log('runInBrowser', runInBrowser);
+
 var correct = 0;
-if(dessert.toLowerCase() === 'cake'){
+if(dessert.toLowerCase() === 'cake') {
+    correct = correct + 1;
+}
+console.log ('results');
+if(runInBrowser.toLowerCase() === 'yes') {
     correct = correct + 1;
 }
 factResults.innerText = 'You got ' + correct + ' out of 5 right';
