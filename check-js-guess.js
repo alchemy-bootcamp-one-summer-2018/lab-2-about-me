176fdd02f6d787a84aaa8687b3affc122198103a
kkrosefields <kkrosefields@gmail.com>
2018-07-25 06:09:38 -0700

    attempted to add more working code

--- check-js-guess.js
+++ check-js-guess.js
@@ -3,24 +3,30 @@
 var guessForm = document.getElementById ("guess-form");
 var guessResult = document.getElementById ("guess-result");
 
-var totalGuess = 0;
+
+var totalGuesses = 0;
+
+
 
 function checkGuessAnswer() {
 
     var guess = guessForm.elements.otter.value;
         if (guess == 'otter') {
-        guessResult.innerText = 'yay!!';
+        guessResult.innerText = 'That\'s right! An ' + guess + ' is my favorite!';
         console.log ('this is what user guessed', guess)
-    
-     }
+        }
 
     else {
-        guessResult.innerText = 'Wrong';
 
         totalGuesses = totalGuesses + 1;
         console.log (totalGuesses)
+        guessResult.innerText = 'Womp Womp... a ' + guess + 
+        ' is not my favorite. You\'re closer though. You\'ve guessed ' + 
+        totalGuesses + ' times try again!'
     }
 
+    
+    
     return false;
 
 }
