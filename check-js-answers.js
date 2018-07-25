a815cdd87307e45ded472a82a590f71277f18cd6
kkrosefields <kkrosefields@gmail.com>
2018-07-24 17:18:53 -0700

    final

--- check-js-answers.js
+++ check-js-answers.js
@@ -3,8 +3,8 @@
 
 var factForm = document.getElementById("fact-form");
 var factSubmit = document.getElementById("js-submit");
-var factResult = document.getElementById("js-form");
-var factRest = document.getElementById ("js-reset")
+var factResult = document.getElementById("js-results");
+var factReset = document.getElementById("js-reset");
 
 
 
@@ -12,29 +12,34 @@ function checkJSAnswers () {
 console.log ('checkJSAnswers called') 
 
     var elements = factForm.elements;
-    var school = elements.school.value
+    var school = elements.school.value;
+    var mtv = elements.mtv.value;
+    var state = elements.state.value
+    var career = elements.career.value
+
     console.log('school var', school);
 
+    var correct = 0;
 
-// need to learn more about this
-
-var correct = correct
-
-    if(school === 'yes') {
+    if (school  === 'yes') {
     correct = correct + 1;
     }   
 
-//result
+    if (mtv || state || career === 'yes') {
+        correct = correct + 1;
+        }   
 
-factResult.innerText ('wow you got ' + correct + ' out 1 right');
 
-factSubmit.disabled = true;
- 
+factResult.innerText = 'wow you got ' + correct + ' out of 5 right';
+factSubmit.disabled = true; 
 factReset.disabled = false;
-
 
 return false;
 
 }
 
-
+function resetJSAnswers() {
+    factResult.innerText = '';
+    factSubmit.disabled = false;
+    factReset.disabled = true;
+}
