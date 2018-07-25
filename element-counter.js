2fb982cc18b9ba4edf928d6bd7788cf8bc5bf585
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-07-25 15:57:10 -0700

    loop one complete

--- element-counter.js
+++ element-counter.js
@@ -1,14 +1,22 @@
 /*exported countTags*/
-
 function countTags() {
     var tags = document.querySelectorAll('input[name=tags]:checked');
     console.log('function works');
     console.log(tags);
-
     var tagCount = {};
-
+    var html = '<ul>';
     for(var i = 0; i < tags.length; i++) {
         console.log(tags[i].value);
+        var tag = tags[i].value;
+        var matches = document.querySelectorAll(tag);
+        console.log(matches.length);
+        tagCount[tag] = matches.length;
+        console.log(tagCount);
+    }
+
+    for(var x = 0; x < tags.length; x++) {
+        var key = Object.keys(tagCount)[x];
+        html += `<li> ${key} ${tagCount[x]} + <li>`;
     }
 
     return false;
