f7e5569b260ce0b6a405a7d688ff4f6d16e4d17a
Claire Flanagan <claireflanagan@gmail.com>
2018-07-25 16:35:33 -0700

    up to 6i

--- checkboxes.js
+++ checkboxes.js
@@ -3,18 +3,27 @@ function countTags() {
     var tags = document.querySelectorAll('input[name=tags]:checked');  //collects checked boxes
     console.log('these are all my tags: ', tags);
     var tagCount = {};   // empty variable to which we will add properties
+
+
+    for(var i = 0; i < tags.length; i++) {
+        var tag = tags[i].value; // this takes the value of a particular tag
+        console.log(tag);
+        var matches = document.querySelectorAll(tag); // get matching tags
+        tagCount[tag] = matches.length; //record result by adding a property to tagCount
+        console.log('this is the tag count', tagCount);
+    }
+
+    var html = '<ul>';
+
+    for(var l = 0; l < tags.length; l++) {
+        console.log('this is tags', tags.name);
+
+    }
+
     return false;
 }
 
-for(var i = 0; i < tags.length; i++) {
-    var tag = tags[i].value; // this takes the value of a particular tag
-    console.log(tag);
-    var matches = document.querySelectorAll(tag);
-    tagCount[tag] = 
-}
-
-
 //var tag = 'p'; //assigns variable? but why?
-//var matches = document.querySelectorAll(tag); 
-//tagCount[tag] = matches.length; 
+//var matches = document.querySelectorAll(tag);
+//tagCount[tag] = matches.length;
 
