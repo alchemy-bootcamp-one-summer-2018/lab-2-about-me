74508c9e5e3bc129821c66a33a25233fe53bd114
warrioryoko <miangpellegri@gmail.com>
2018-07-24 21:23:07 -0700

    Can't quite get the guessing game logic. Too tired, going home.

--- check-guess-answers.js
+++ check-guess-answers.js
@@ -3,6 +3,7 @@
 // get the elements we need to use
 var guessForm = document.getElementById('guess-form');
 var guessResults = document.getElementById('guess-results');
+var guessButton = document.getElementById('guess-button');
 
 // track "state" _across_ calls to checkGuessAnswer
 var totalGuesses = 0;
@@ -10,24 +11,43 @@ var totalGuesses = 0;
 function checkGuessAnswer() {
     var guess = guessForm.elements.number.value;
 
+    
     // eslint-disable-next-line
     if(guess == 7) {
-        // go ahead and end game and let user know they won
+        guessResults.classList.remove(guessResults.classList.item(0));
         guessResults.innerText = 'That\'s right! You win!';
         guessResults.classList.add('correctAnswer');
+        totalGuesses = totalGuesses + 1;
+        console.log(totalGuesses);
     }
     else {
+        guessResults.classList.remove(guessResults.classList.item(0));
+        guessResults.innerText = 'Wrong';
+        guessResults.classList.add('wrongAnswer');
+        totalGuesses = totalGuesses + 1;
+        console.log(totalGuesses);
+    }
+
+    if(totalGuesses > 4) {
+        guessButton.disabled = true;
+        guessResults.innerText = 'You have exceeded the maximum number of guesses... you lose.';
+    }
+
+    
+    
+    /*else {
         while(guessResults.classList.length > 0) {
             guessResults.classList.remove(guessResults.classList.item(0));
         }
         guessResults.innerText = 'Wrong';
         guessResults.classList.add('wrongAnswer');
+        if(totalGuesses =)
         // now we can increment and hold onto the value across
         // calls to this function (this function only increases
         // the value, it doesn't initialize the value)
-        totalGuesses = totalGuesses + 1;
-        console.log(totalGuesses);
-    }
+        totalGuesses = totalGuesses + 1; */
+    console.log(totalGuesses);
+    
 
     return false;
 }
