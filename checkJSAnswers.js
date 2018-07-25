c1f6a29ae21ac6915ef65729c080bc08bba5ba0d
Sophie Graham <sophiergraham1@gmail.com>
2018-07-24 18:45:08 -0700

    question 2

--- checkJSAnswers.js
+++ checkJSAnswers.js
@@ -6,15 +6,23 @@ function checkJSAnswers() {
 
     var elements = factForm.elements;
     var hometown = elements.hometown.value;
+    var runInBrowser = elements.runInBrowser.value;
  
     console.log('hometown', hometown);
+    console.log('runInBrowser', runInBrowser);
     
     var correct = 0;
 
     if(hometown.toLowerCase() === 'portland') {
         correct = correct + 1;
     }
-    console.log('results')
+    if(runInBrowser.toLowerCase() === 'yes') {
+        correct = correct + 1;
+    }
+    console.log('results1')
     factResults.innerText = 'You got ' + correct + ' out of 5 right';
     return false;
-}
+    
+    
+    }
+    
