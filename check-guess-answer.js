e4cb46ed871a69839111485b265ac9b74c13a23e
Sarah Flynn <sarah.flynn@icloud.com>
2018-07-24 18:02:08 -0700

    both basic games working for first time

--- check-guess-answer.js
+++ check-guess-answer.js
@@ -1,8 +1,11 @@
+// Creating global var for guessCount and setting to zero
 var guessCount = 0;
 
 // Getting button elements for enabling/disabling
 var guessSubmit = document.getElementById('guess-submit');
 var guessReset = document.getElementById('guess-reset');
+
+// Setting initial value of winning number
 var winningNumber = Math.floor(Math.random() * 100) + 1;
 
 function checkGuess() {
@@ -10,18 +13,30 @@ function checkGuess() {
     console.log('winningNumber: ', winningNumber)
 
     var guessingGame = document.getElementById('guessing-game');
-
     var elements = guessingGame.elements; 
     var guess = elements.guess.value;
+    var feedback = document.getElementById('guessFeedback');
     console.log('guess: ', guess);
     console.log('guessCountBefore: ', guessCount);
     guessCount++;
     console.log('guessCountAfter: ', guessCount);
+    console.log('feedback: ', feedback);
+
+    if (guess > winningNumber) {
+        feedback.innerText = 'The winning number is lower than ' + guess;
+    } else if (guess < winningNumber) {
+        feedback.innerText = 'The winning number is higher than ' + guess;
+    } else if (parseInt(guess) === winningNumber) {
+        feedback.innerText = 'You Win!!! ' + guess + ' is right, and you got the answer in only ' + guessCount + ' guesses.';
+        // Disable the submit button
+        guessSubmit.disabled = true;
+        // Enable the reset button
+        guessReset.disabled = false;
+    }
 
     if (guessCount > 9) {
         // Disable the submit button
         guessSubmit.disabled = true;
-
         // Enable the reset button
         guessReset.disabled = false;
     }
