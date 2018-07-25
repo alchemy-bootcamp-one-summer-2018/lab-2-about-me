c4333cdf2e881065c9131aa3ea0e20a798b0b7f3
Jack <ajackofsometrades@gmail.com>
2018-07-25 15:00:05 -0700

    meeting requirements

--- element-counter.js
+++ element-counter.js
@@ -4,5 +4,33 @@ function countElements() {
 
     console.log('I work');
 
+    var tags = document.querySelectorAll('input[name=tags]:checked');
+    var tagCount = {}
+
+    for (i = 0; i < tags.length; i++) {         
+        tag = tags[i].value;
+        tagCount[tag] = document.querySelectorAll(tag).length;
+        console.log(tagCount);
+    }
+
+    var html = '<ul>';
+
+    /*
+    for (i = 0; i < tags.length; i++) {
+        tag = tags[i].value;
+        document.querySelectorAll(tag).length;
+        console.log(tagCount);
+    }
+    */
+
+    for (var key in tagCount){
+        html += `<li>${key} ${tagCount[key]}</li>`;
+    }
+
+    html += ('</ul>');
+    document.getElementById('elements-results').innerHTML = html;
+
     return false;
 }
+
+
