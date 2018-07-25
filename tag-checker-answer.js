e65eed473f7e638954fe923d3be4e676d880aae2
Alex Rankin <alex.c.rankin@gmail.com>
2018-07-25 14:20:05 -0700

    moved variables inside function, now adding to nodelist

--- tag-checker-answer.js
+++ tag-checker-answer.js
@@ -1,13 +1,17 @@
 /* exported tagCounter */
 
-tagCounter = function(){
-    console.log('hello');
 
+tagCounter = function(){
+    var tags = document.querySelectorAll('input[name=tags]:checked');
+    
+    //var tagCount = {};
+
+    console.log('hello');
+    console.log(tags);
+    
+    
     return false;
 };
 
-var tags = document.querySelectorAll('input[name=tags]:checked');
-
-var tagCount = {};
 
 
