7ef5daad61ceb7fab5d643017c8e2e86a32075d8
Mike <mbennettillustration@gmail.com>
2018-07-25 17:30:54 -0700

    final commit

--- check-checkboxes.js
+++ check-checkboxes.js
@@ -1,3 +1,4 @@
+
 function countTags() {
     var tags = document.querySelectorAll('input[name="tags"]:checked');
     console.log('Showing', tags);
@@ -10,10 +11,16 @@ function countTags() {
         console.log('What are', matches);
         tagCount[tag] = matches.length;
         console.log('How many', tagCount);
-    }
+    }  
+    var html = '<ul>';
+    for(var key in tagCount) {
+        console.log('key', key);
+        console.log('again', tagCount[key]);
+        html += '<li>' + key + tagCount[key] + '</li>';
+    }   
+    html += '</ul>';
+    console.log('html', html);
+    var results = document.getElementById('results');
+    results.innerHTML = html;
     return false;
-}
-var l;
-for(i = 0; l < tagCount; l++) {
-    
 }
