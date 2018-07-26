d1cb5ebd694d9dc1fc8f3e9d4570e37136d680b4
warrioryoko <miangpellegri@gmail.com>
2018-07-26 08:42:19 -0700

    Added changes in check-guesses

--- check-guess-answers.js
+++ check-guess-answers.js
@@ -10,8 +10,6 @@ var totalGuesses = 0;
 
 function checkGuessAnswer() {
     var guess = guessForm.elements.number.value;
-
-    
     // eslint-disable-next-line
     if(guess == 7) {
         guessResults.classList.remove(guessResults.classList.item(0));
@@ -32,9 +30,6 @@ function checkGuessAnswer() {
         guessButton.disabled = true;
         guessResults.innerText = 'You have exceeded the maximum number of guesses... you lose.';
     }
-
-    
-    
     /*else {
         while(guessResults.classList.length > 0) {
             guessResults.classList.remove(guessResults.classList.item(0));
@@ -47,7 +42,6 @@ function checkGuessAnswer() {
         // the value, it doesn't initialize the value)
         totalGuesses = totalGuesses + 1; */
     console.log(totalGuesses);
-    
 
     return false;
 }
