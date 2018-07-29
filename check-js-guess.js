a62923049c83e2c826696a0176ee82087ce6ef70
kkrosefields <kkrosefields@gmail.com>
2018-07-29 14:53:22 -0700

    add a line of code so user can now see how many tries they have left

--- check-js-guess.js
+++ check-js-guess.js
@@ -4,6 +4,8 @@ var guessForm = document.getElementById ('guess-form');
 var guessResult = document.getElementById ('guess-result');
 
 var totalGuesses = 0;
+var lives = 5;
+
 function checkGuessAnswer(){
 
     var guess = guessForm.elements.otter.value;
@@ -16,9 +18,9 @@ function checkGuessAnswer(){
 
         totalGuesses = totalGuesses + 1;
         console.log (totalGuesses);
+        lives--;
         guessResult.innerText = 'Whomp Whomp... a ' + guess +
-        ' is not my favorite. You\'re closer though. You\'ve guessed ' +
-        totalGuesses + ' times try again!';
+        ' is not my favorite. You\'re closer though. You have ' + lives + ' more tries!';
     }
 
     return false;
